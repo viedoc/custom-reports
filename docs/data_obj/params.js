453216a57654720186c1481faefe3eff3ee68929
syllybelle <60044258+syllybelle@users.noreply.github.com>
2025-04-14 13:14:30 +0200

    Create params.js

---
+++ docs/data_obj/params.js
@@ -0,0 +1,31 @@
+{
+  UserDetails: {
+    studyinfo: {
+      studyName: "reports_download";
+      studyType: "Training";
+    }
+    sites: [
+      {
+        siteNumber: 1,
+        siteCode: "1",
+        siteName: "1",
+        countryCode: "SE",
+        country: "Sweden",
+        timeZone: "W. Europe Standard Time",
+        tzOffset: 60,
+        siteType: "Training",
+        expectedNumberOfSubjectsScreened: "NA",
+        expectedNumberOfSubjectsEnrolled: "NA",
+        maximumNumberOfSubjectsScreened: "NA",
+      },
+    ];
+    studysettings: {
+      expectedNumberOfScreenedSubjects: null;
+      expectedNumberOfEnrolledSubjects: null;
+      expectedDateOfCompleteEnrollment: null;
+      totalNumberOfStudySites: 1;
+      totalNumberOfUniqueCountries: 1;
+    }
+  }
+  dateOfDownload: "2025-03-25 15:51:30";
+}
